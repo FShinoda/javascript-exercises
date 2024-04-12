edfddd34db3b6d21614021e96d08fcc97b7957cd
FShinoda <fernanda.shinoda@hotmail.com>
2024-04-12 17:39:23 -0300

    feat: remove from array using delete
    - delete is used to arrayToBeDeleted. The remaining undefined values that stays as 'holes' in the array are filtered in a second function filterArrayGaps

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,29 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToBeModified, ...args) {
+    // Doing without any built-in methods
+    for(let i = 0; i < arrayToBeModified.length; i++){
+        for(let j = 0; j < args.length; j++){
+            if (arrayToBeModified[i] === args[j]){
+                delete arrayToBeModified[i];
+            }
+        }
+    }
 
+    return filterArrayGaps(arrayToBeModified);
 };
+
+function filterArrayGaps(arrayToBeModified){
+    let filteredArray = [];
+
+    for (let i = 0; i < arrayToBeModified.length; i++){
+        if (arrayToBeModified[i] !== undefined){
+            filteredArray.push(arrayToBeModified[i]);
+        }
+    }
+
+    return filteredArray;
+}
+
+removeFromArray([1, 2, 3, 4], 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
