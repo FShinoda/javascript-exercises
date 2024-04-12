9de5703f17023189660e571dc6202a664ca868e5
FShinoda <fernanda.shinoda@hotmail.com>
2024-04-12 17:38:15 -0300

    feat: reverse string with for loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(stringToBeReversed) {
+    let reversedString = '';
 
+    for (let i = 0; i < stringToBeReversed.length; i++){
+        reversedString += stringToBeReversed[(stringToBeReversed.length-1)-i];
+    };
+
+    return reversedString;
 };
 
 // Do not edit below this line
