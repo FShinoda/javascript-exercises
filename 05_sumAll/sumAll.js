b0343683e11c18335622d97aa928e043710f13d8
FShinoda <fernanda.shinoda@hotmail.com>
2024-04-16 11:39:08 -0300

    feat: sum all numbers between two using for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(floor, ceil) {
+    if (typeof floor != "number" || typeof ceil != "number" || floor < 0 || ceil < 0) {
+        return "ERROR";
+    };
+    
+    if(ceil < floor){
+        let auxExchange = ceil;
+        ceil = floor;
+        floor = auxExchange;
+    };
 
+
+    let sumTotal = 0;
+
+    for (let i = floor; i <= ceil; i++){
+        sumTotal += i;
+    }
+
+    return sumTotal;
 };
 
 // Do not edit below this line
