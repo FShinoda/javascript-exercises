7c4ff56ec6483f734f09a3d315cb79374bc2241c
FShinoda <fernanda.shinoda@hotmail.com>
2024-05-02 16:20:36 -0300

    feat: use of sort array method to get oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((first, next) => {
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
+        if(first["yearOfDeath"] === undefined){
+            first["yearOfDeath"] = currentYear;
+        };
+        if(next["yearOfDeath"] === undefined){
+            next["yearOfDeath"] = currentYear;
 
+        };
+
+        const ageOfFirst = first["yearOfDeath"] - first["yearOfBirth"];
+        const ageOfNext = next["yearOfDeath"] - next["yearOfBirth"];
+
+        return ageOfFirst < ageOfNext ? 1 : -1;
+    })
+
+    return people[0];
 };
 
 // Do not edit below this line
