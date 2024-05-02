6e4d486ec09e4d844076a9007105fb4bcc042595
FShinoda <fernanda.shinoda@hotmail.com>
2024-05-02 14:39:59 -0300

    feat: palindromes function done
    - done function for removing punctuation and spaces
    - done function for reversing the string
    - check palindromes as strings, not arrays

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+function removePunctuationWordBreaks(word){
+    // Ponctuations
+    const ponctuations = ['.', ',', ';', '!', '?'];
+    ponctuations.forEach(p => {
+        word = word.replaceAll(p, '');
+        console.log(p);
+        console.log(word);
+        
+    });
 
+    // Blank Spaces
+    word = word.replaceAll(' ', '');
+    console.log('removed blanks: ', word)
+
+    return word;
+};
+
+function reverseString(word) {
+    const reversed = word.split('').reverse().join('');
+    return reversed;
+}
+
+const palindromes = function(word) {
+    word = word.toLowerCase();
+    word = removePunctuationWordBreaks(word);
+
+    return word == reverseString(word) ? true : false;
 };
 
 // Do not edit below this line
